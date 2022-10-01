5f1f0f09cf38c77ab8b3720069c8435bd600c274
Juanperezaa <sebas8041@hotmail.com>
2022-10-01 01:56:29 -0500

    Final partition almost implemented, just left P=P+1 validation

--- src/main/java/com/edu/icesi/jfxautomatongeneration/model/MooreMachine.java
+++ src/main/java/com/edu/icesi/jfxautomatongeneration/model/MooreMachine.java
@@ -11,35 +11,35 @@ public class MooreMachine {
     private ArrayList<String> combinations = new ArrayList<>();
 
     public ArrayList<ArrayList<Integer>> partitioning = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> fPartitioning = new ArrayList<>();
 
 
-    public MooreMachine(ArrayList<ArrayList<Integer>> states, ArrayList<ArrayList<Integer>> outPuts){
-        this.states=states;
-        checkList=new ArrayList<>();
-        this.outPuts=outPuts;
+    public MooreMachine(ArrayList<ArrayList<Integer>> states, ArrayList<ArrayList<Integer>> outPuts) {
+        this.states = states;
+        checkList = new ArrayList<>();
+        this.outPuts = outPuts;
         checkList.add(0);
-        tablesEnd=states.get(0).size();
+        tablesEnd = states.get(0).size();
         machineProcess();
 
     }
 
-    public void machineProcess(){
+    public void machineProcess() {
         isConnectedToRoot();
         deleteStatesRootUnconnected();
         createInitialPartition();
     }
 
-    public void createInitialPartition(){
-        for(int i=0;i<outPuts.size();i++){
+    public void createInitialPartition() {
+        for (int i = 0; i < outPuts.size(); i++) {
             String combination = "";
-            for(int j=1;j<outPuts.get(i).size();j++){
-                combination += outPuts.get(i).get(j)+"";
+            for (int j = 1; j < outPuts.get(i).size(); j++) {
+                combination += outPuts.get(i).get(j) + "";
             }
-            int index = isInCombinations(combinations,combination);
-            if(index!=-1){
+            int index = isInCombinations(combinations, combination);
+            if (index != -1) {
                 partitioning.get(index).add(outPuts.get(i).get(0));
-            }
-            else{
+            } else {
                 combinations.add(combination);
                 ArrayList<Integer> newCombination = new ArrayList<>();
                 newCombination.add(outPuts.get(i).get(0));
@@ -49,19 +49,19 @@ public class MooreMachine {
         showPartitioning();
     }
 
-    private void showPartitioning(){
-        for(int i=0;i<partitioning.size();i++){
-            System.out.println("Combination: "+combinations.get(i));
-            System.out.println("Elements");
-            for (int j=0;j<partitioning.get(i).size();j++){
+    private void showPartitioning() {
+        for (int i = 0; i < partitioning.size(); i++) {
+            System.out.println("Combination: " + combinations.get(i));
+            System.out.println("Elements" + combinations.get(i).length());
+            for (int j = 0; j < partitioning.get(i).size(); j++) {
                 System.out.println(partitioning.get(i).get(j));
             }
         }
     }
 
-    private int isInCombinations(ArrayList<String> combinations,String combination){
-        for(int i=0;i<combinations.size();i++){
-            if(combinations.get(i).equals(combination)){
+    private int isInCombinations(ArrayList<String> combinations, String combination) {
+        for (int i = 0; i < combinations.size(); i++) {
+            if (combinations.get(i).equals(combination)) {
                 return i;
             }
         }
@@ -69,11 +69,11 @@ public class MooreMachine {
     }
 
 
-    public void deleteStatesRootUnconnected(){
+    public void deleteStatesRootUnconnected() {
         Collections.sort(checkList);
-        for(int i=0;i<states.size();i++){
-            if(!searchState(i)){
-                System.out.println("Removed "+i);
+        for (int i = 0; i < states.size(); i++) {
+            if (!searchState(i)) {
+                System.out.println("Removed " + i);
                 states.remove(i);
                 outPuts.remove(i);
             }
@@ -82,28 +82,27 @@ public class MooreMachine {
 
 
     //Recorrer matriz, seleccionar nodos conectados a la raiz
-    public void isConnectedToRoot(){
-         isConnectedToRoot(0, 0);
-        for(int j=0;j<checkList.size();j++){
-            System.out.println(checkList.get(j));
+    public void isConnectedToRoot() {
+        isConnectedToRoot(0, 0);
+        for (int j = 0; j < checkList.size(); j++) {
+            // System.out.println(checkList.get(j));
         }
     }
 
-    private void isConnectedToRoot(int c, int r){
-        if((c+1)<tablesEnd){ //sus
-            int index=states.get(r).get(c+1); //sus
+    private void isConnectedToRoot(int c, int r) {
+        if ((c + 1) < tablesEnd) { //sus
+            int index = states.get(r).get(c + 1); //sus
             //System.out.println(index+"a probar");
             //System.out.println("Columna :"+c+" Fila :"+r);
-            if (!searchState(index)){
+            if (!searchState(index)) {
                 //System.out.println("C :"+c+" F :"+r);
                 //System.out.println("No está: "+index);
                 checkList.add(index);
-                isConnectedToRoot(0,index);
-                isConnectedToRoot(c+1,r);
-            }
-            else {
+                isConnectedToRoot(0, index);
+                isConnectedToRoot(c + 1, r);
+            } else {
                 //System.out.println("Ya está: " + index);
-                isConnectedToRoot(c+1, r);
+                isConnectedToRoot(c + 1, r);
                 //System.out.println("MUERTE");
                 //System.out.println("C :"+c+" F :"+r);
             }
@@ -113,18 +112,107 @@ public class MooreMachine {
         }
          */
     }
-    private boolean searchState(int state){
-        for(int i=0;i<checkList.size();i++){
-            if (checkList.get(i)==state){
+
+    private boolean searchState(int state) {
+        for (int i = 0; i < checkList.size(); i++) {
+            if (checkList.get(i) == state) {
                 return true;
             }
         }
         return false;
     }
 
-    public void matrixCreation(ArrayList<String> states, String checkList, int i){
 
+    //Que pare cuando P=P+1,es sencillo solo que ando en otro mundo a esta hora.
+    public void finalPartition(){
+        while (partitioning!=makingPartition()){
+
+        }
     }
+
+    public ArrayList<ArrayList<Integer>> makingPartition() {
+        ArrayList<ArrayList<Integer>> newPartition=new ArrayList<>();
+        for (int i = 0; i < partitioning.size(); i++) {
+            setNewRows(newPartition, partitionVerifier(partitioning.get(i)));
+        }
+        return newPartition;
+    }
+    public void setNewRows(ArrayList<ArrayList<Integer>> original, ArrayList<ArrayList<Integer>> additional){
+        for(int i=0;i<additional.size();i++){
+            original.add(additional.get(i));
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> partitionVerifier(ArrayList<Integer> set){
+        ArrayList<ArrayList<Integer>> newRows=new ArrayList<>();
+        ArrayList<Integer> alreadyGrouped=new ArrayList<>();
+        int rowsCount=0;
+        for (int i=0;i<set.size();i++){
+            int state=set.get(i);
+            if(!isGrouped(alreadyGrouped,state)) {
+                //VERIFICAR OUTPUTS DE STATE
+                for (int j = 1; j < set.size(); j++) {
+                    if (verifyOutputs(state, set.get(j))) {
+                        if (!containsTheState(newRows, state)) {
+                            newRows.get(rowsCount).add(state);
+                            alreadyGrouped.add(state);
+                        }
+                        newRows.get(rowsCount).add(set.get(j));
+                        alreadyGrouped.add(set.get(j));
+                    }
+                }
+                if(!containsTheState(newRows,state)){
+                    newRows.get(rowsCount).add(state);
+                }
+                rowsCount++;
+            }
+        }
+        return newRows;
+    }
+
+    public boolean verifyOutputs(int stateToProve, int anotherState){
+        //El valor cero de outputs al parecer es su indice
+        for (int i=1;i<outPuts.size();i++){
+            int testStatePos=statePartitionPos(outPuts.get(stateToProve).get(i));
+            int anotherStatePos=statePartitionPos(outPuts.get(anotherState).get(i));
+            if(!(testStatePos==anotherStatePos)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int statePartitionPos(int stateToProve){
+        int whereIsState=-1;
+        for (int i=0;i< partitioning.size();i++){
+            for (int j=0;j<partitioning.get(i).size();j++){
+                if(stateToProve==partitioning.get(i).get(j)){
+                    whereIsState=i;
+                }
+            }
+        }
+        return whereIsState;
+    }
+
+    public boolean containsTheState(ArrayList<ArrayList<Integer>> newRows, int stateToProve){
+        for (int i=0;i<newRows.size();i++){
+            for (int j=0;j<newRows.get(i).size();j++){
+                if(stateToProve==newRows.get(i).get(j)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean isGrouped(ArrayList<Integer> aux, int test){
+        for (int i=0;i<aux.size();i++){
+            if(aux.get(i)==test){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public ArrayList<String> subGrouping(ArrayList<ArrayList<String>> states){
         return states.get(0);
